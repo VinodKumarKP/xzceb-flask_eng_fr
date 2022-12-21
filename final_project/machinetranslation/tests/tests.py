36dd34ddf373d160c789913f4f7da0c4b4b7405e
Vinod Kumar <vinodkumar@example.com>
2022-12-21 00:24:26 -0500

    moved test to new directory

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,18 @@
+import unittest
+from translator import french_to_english, english_to_french
+
+
+class TestFrenchtoEnglish(unittest.TestCase):
+    def test_null(self):
+        self.assertEqual(french_to_english(None), '')
+
+    def test_translate(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+
+class TestEnglishtoFrench(unittest.TestCase):
+    def test_null(self):
+        self.assertEqual(english_to_french(None), '')
+
+    def test_translate(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
